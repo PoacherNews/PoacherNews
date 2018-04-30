5a843549fb3002e93236ef1ae76e1e2f7b8f7a3a
bypxtd <okayfinebruce@gmail.com>
2018-04-29 21:31:47 -0500

    Updated link redirects and linked database

--- util/db.php
+++ util/db.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', '');
+define('DB_SERVER', 'poacherdatabase.ccbtf4xhozoc.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'mysqladmin');
+define('DB_PASSWORD', 'Hunter1234');
+define('DB_DATABASE', 'PoacherNews');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
