76b91f2a0eab3b23eb7c998470406126d3462372
Matthew Barber <mgbz89@mail.missouri.edu>
2018-04-30 13:06:58 -0500

    Added error.php to includes

---
+++ includes/error.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Session Error</title>
+	<style>
+        body {
+            text-align: center;
+        }
+		.error {
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<h1 class="error">Error</h1>
+	<p>There was an error starting a session.</p>
+    <p>Please contact the system administrator.</p>
+</body>
+</html>
