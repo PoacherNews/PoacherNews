d122a1e41b2ae7ddee2ee2d2b7e42d7ee8ef6ef3
Colin Stevens <colin@colinjstevens.com>
2018-05-01 16:54:03 -0500

    Added initial article functionality

--- article.php
+++ article.php
@@ -2,6 +2,7 @@
 <html>
     <head>
 	<?php include 'includes/globalHead.html' ?>
+    <link rel="stylesheet" type="text/css" href="res/css/articlepage.css">
     </head>
     <body>
         <?php 
@@ -10,39 +11,92 @@
             //include 'includes/footer.html';
         ?>
         
-        <div class="apDivLeft"></div> 
-        <div class="apDivRight"></div>
-        <br>
-        <div class="apDivArt">
-            <div class="apDivPic">
-                <h2 class="apHead2">Image Placeholder</h2>
-            </div>
-            <h1 class="apHead1">Title Placeholder</h1>
-            <br><br>
-            <p class="apPArt">This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. This is the content of the article. </p>
+        <?php include('util/db.php'); ?>
+        <?php 
+            //echo "ARTICLE ID IN URL: ".$_GET['articleid']
+            date_default_timezone_set('America/Chicago');
+            $sql = "SELECT * FROM Articles WHERE ArticleID = ".$_GET['articleid'];
+            $result = mysqli_query($db, $sql);
+            if(mysqli_num_rows($result) == 0) {
+                print "Error! ArticleID not found!";
+            }
+            $articleData = mysqli_fetch_assoc($result);
+        ?>
+        <div class="articleColumns">
+            <section id="articleColumn">
+                <span class="articleDetails">
+                    <h1><?php print $articleData['Headline'] ?></h1>
+                    <p><?php print $articleData['Author'] ?></p>
+                    <p>Published on <?php print date(DATE_RFC850, $articleData['PublishDate'])." &mdash; ".$articleData['Views']." Views"; ?></p>
+                </span>
+                <div class="articleImage">
+                    <img src="<?php print $articleData['Img'] ?>"/>
+                </div>
+                <div class="articleBody">
+                    <p><?php print $articleData['Body'] ?></p>
+                </div>
+            </section>
+            <section id="adColumn">
+                <div class="ad"><p>AD</p></div>
+                <div class="ad"><p>AD</p></div>
+                <div class="ad"><p>AD</p></div>
+            </section>
         </div>
-        <hr class="apHr">
-        <div class="apDivBannerAds">Advertisement Placeholder</div>
-        <hr class="apHr">
-        <div class="apDivSideAds">Advertisement Placeholder</div>
-        <div class="apDivMid"> </div>
-        <!-- THUMBNAIL ARTICLES-->
-        <div class="apDivHoldTnArt"> 
-            <div class="apDivTn">Thumbnail Placeholder</div>
-            <h1 class="apHead1">Title Placeholder</h1>
-            <p class="apArtContent">This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. </p>
+        <div id="further-reading">
+            <h1>Further reading</h1>
+            <hr/>
         </div>
-        
-        <div class="apDivHoldTnArt"> 
-            <div class="apDivTn">Thumbnail Placeholder</div>
-            <h1 class="apHead1">Title Placeholder</h1>
-            <p class="apArtContent">This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. </p>
+        <div class="articleColumns">
+            <?php echo "Article cat: ".$articleData['Views']; ?>
+            <section class="threeColumnSection">
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+            </section>
+            <section class="threeColumnSection center">
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+            </section>
+            <section class="threeColumnSection">
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+                <article>
+                    <div class="articleBody">
+                        <h1>Headline</h1>
+                        <p>Foo</p>
+                        <a class="continue-reading" href="#">Continue Reading</a>
+                    </div>
+                </article>
+            </section>
         </div>
-        
-        <div class="apDivHoldTnArt"> 
-            <div class="apDivTn">Thumbnail Placeholder</div>
-            <h1 class="apHead1">Title Placeholder</h1>
-            <p class="apArtContent">This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. This is some content in the thumbnail. </p>
-        </div>
+
     </body>
 </html>
