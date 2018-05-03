ff04ee85bd585d622ed1fb44b35966b6b375cbf3
Oruche <rro2q2@mail.missouri.edu>
2018-05-03 16:24:32 -0500

    Still fixing search.php

--- search.php
+++ search.php
@@ -13,8 +13,8 @@
         <div class="flexRow">
             <div class="searchFlex">
                 <h2>Search Results</h2>
-                <form class="search" action="">
-                    <input type="text" id="searchInput" placeholder="What are you looking for?" name="searchBar">
+                <form class="search" action="search.php" method="GET">
+                    <input type="text" id="searchInput" placeholder="What are you looking for?" name="query">
                     <button type="submit"><i class="fa fa-search"></i></button>
                     <p id="filter">Filter</p>
                 </form>
@@ -45,13 +45,27 @@
         <?php
         /*Include standard database*/
         include('includes/footer.html');
-        include('dp.php');
+        include('util/db.php');
         if($db->connect_error) {
             die("Connection failed: ". $db->connect_error);
         }
+        $query = $_GET['query'];
         
+        $min_length = 3;
         
+        if(strlen($query) >= $min_length) {
+            $query = htmlspecialchars($query);
+            
+            $query = mysql_real_escape_string($query);
+
+            $raw_results = mysql_query("SELECT * FROM Articles WHERE ('Headline' LIKE  '%".$query."%')") or die(mysql_eror());
+        }
         
+        if(mysql_num_rows($raw_results) > 0) {
+            while($results = mysql_fetch_array($raw_results)) {
+                echo "<p><h3>".$results['Headline']."</h3></p>";
+            }
+        } 
         ?>  
     </body>
 </html>
