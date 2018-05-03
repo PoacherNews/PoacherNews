26768abf98ed66a97e81aa22564cdc9dede2f9c2
Colin Stevens <colin@colinjstevens.com>
2018-05-03 14:12:38 -0500

    Fixed session_start warnings

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,6 @@
         <a href="search.php"><i class="fa fa-search"></i></a>
         <a href="login.php"><i class="fa fa-user-circle"></i></a>
         <?php
-            session_start();
             if($_SESSION['loggedin']) {
                 print "<a href=\"logout.php\"><i class=\"fa fa-sign-out\"></i></a>";
                 print "<a class=\"username\" href=\"#\">{$_SESSION['username']}</a>"; //TODO: Link to user's userpage 
