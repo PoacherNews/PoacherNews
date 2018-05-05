d7cc8341e42bc0f650f836cee4258679af885841
Roland Oruche <rro2q2@mail.missouri.edu>
2018-05-05 15:26:53 -0500

    Add files via upload

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,10 @@
     <ul class="navUl">
         <div class="navWrapper">
-            <li class="navLi"><a class="navA" href="/section.php">Politics</a></li>
-            <li class="navLi" ><a class="navA" href="/section.php">Sports</a></li>
-            <li class="navLi" ><a class="navA" href="/section.php">Entertainment</a></li>
-            <li class="navLi" ><a class="navA" href="/section.php">Video</a></li>
-            <li class="navLi" ><a class="navA" href="/section.php">Local</a></li>
-            <li  class="navLi" ><a class="navA" href="/section.php">Opinion</a></li>
+            <li class="navLi"><a class="navA" href="section.php">Politics</a></li>
+            <li class="navLi" ><a class="navA" href="section.php">Sports</a></li>
+            <li class="navLi" ><a class="navA" href="section.php">Entertainment</a></li>
+            <li class="navLi" ><a class="navA" href="section.php">Video</a></li>
+            <li class="navLi" ><a class="navA" href="section.php">Local</a></li>
+            <li  class="navLi" ><a class="navA" href="section.php">Opinion</a></li>
         </div>
     </ul>
