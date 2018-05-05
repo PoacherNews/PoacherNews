6507c874f954bd33cbc24725ec2ae3fe4e9d0530
Roland Oruche <rro2q2@mail.missouri.edu>
2018-05-05 15:26:18 -0500

    Add files via upload

--- section.php
+++ section.php
@@ -1,164 +1,152 @@
-<!DOCTYPE>
+<?php session_start(); ?>
+<!-- TODO: 
+Fix css (Resizing issues / min-width)
+-->
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Search results</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="res/css/search.css">
+    <head>
+	   <?php include 'includes/globalHead.html' ?>
+    </head>
     
-    <?php include 'includes/globalHead.html' ?>
-    <?php
-    mysql_connect("poacherdatabase.ccbtf4xhozoc.us-east-2.rds.amazonaws.com", "mysqladmin", "Hunter1234") or die("Error connecting to database: ".mysql_error());
-         
-    mysql_select_db("PoacherNews") or die(mysql_error());
-    ?>
-</head>
-<body>
-    <?php
-        include 'includes/header.php';
-        include 'includes/nav.php';
-    ?>
-    <div class="flexRow">
-        <div class="searchFlex">
-            <h1>Search Results</h1>
-            <form class="search" action="" method="GET">
-                <input type="text" id="searchInput" placeholder="What are you looking for?" name="query"/>
-                <div class="buttonFlex">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </div>
-            </form>
-        </div>
-        <div class="dropdown">
-            <div class="dropbtn">Sort by 
-                <i class="fa fa-caret-down"></i>
-            </div>
-            <div class="dropdown-content">
-                <a href="search.php?sort=Relevancy">Relevancy</a>
-                <a href="search.php?sort=Name">Name</a>
-                <a href="search.php?sort=Views">Views</a>
-            </div>
-        </div> 
+    <body>
+        <?php 
+            include 'includes/header.php';
+            include 'includes/nav.php';
+        ?>
         
-    </div>
-    
-    <div class="resultFlex">
-            <div class="testFlex">
-            <?php
-                $query = $_GET['query'];
-                $sort = $_GET['sort'];
-                $min_length = 1;
-                $newResults = array();
-                if(strlen($query) >= $min_length){ 
-                    $query = htmlspecialchars($query); 
-                    $query = mysql_real_escape_string($query);
-                    $raw_results = mysql_query("SELECT * FROM Articles
-                    WHERE (`Headline` LIKE '%".$query."%') OR (`Img` LIKE '%".$query."%')") or  die(mysql_error());
-                    echo "<div id='divTest'>";
-                    if(mysql_num_rows($raw_results) > 0){ 
-                        while($results = mysql_fetch_array($raw_results)){
-                            print   "<div class='flexRow'>
-                                        <div class='imgFlex'>
-                                            <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                <img src=".$results['Img']." class='image' height='120' width='140'>
-                                            </a>
-                                        </div>
-                                        <div class='spanFlex'>
-                                            <span class='tip'>
-                                                <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <hr class='searchHr'>";   
-                        }
-                    }
-                    else{
-                        print "<b>No results.</b>";
-                    }
-                    
-                }
-                echo "</div>";
-                // TESTING THE SORT FUNCTION
-                if($query === is_null) {
-                    echo "Nothing to be sorted...";
-                } else {
-                    switch($sort) {
-                        case "Relevancy":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY PublishDate DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            
-                            break;
-                            
-                        case "Name":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Headline DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            break;
-                            
-                        case "Views":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Views DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            break;
-                    }
-                }
-            ?>
+        <p id="secName">Section</p>
+
+        <!-- containerPrimary Head -->
+        <section class="secPrimary">
+        <div class="secContainerPrimary">
+        <div class="secRowPrimary">
+            <div class="secBorderPrimary"></div>
+
+            <div class="secColumnPrimary">
+            <article>
+                <div class="thumbnailPrimary">
+                    <img src="moon.jpg" width="350" height="250">
+                </div>
+                <div class="textPrimary">
+                    <h1 class="secHeadlinePrimary"><a href="">(Headline Placeholder)</a></h1>
+                    <p>(Filler Text Filler Text Filler Text)</p>
+                </div>
+            </article>
+            
+            <article>
+                <div class="thumbnailPrimary">
+                    <img src="" width="350" height="250">
+                </div>
+                <div class="textPrimary">
+                    <h1 class="secHeadlinePrimary"><a href="">(Headline Placeholder)</a></h1>
+                    <p>(Filler Text Filler Text Filler Text)</p>
+                </div>
+            </article>    
+                
+            <article>
+                <div class="thumbnailPrimary">
+                    <img src="" width="350" height="250">
+                </div>
+                <div class="textPrimary">
+                    <h1 class="secHeadlinePrimary"><a href="">(Headline Placeholder)</a></h1>
+                    <p>(Filler Text Filler Text Filler Text)</p>
+                </div>
+            </article>
+
+            <!-- Show More -->
+            <div class="showMore">
+                <a href="">Show More</a>
             </div>
+                
+            </div>
+            <div class="secBorderPrimary"></div>
+        </div>
+        </div>
+        </section>
+        <!-- containerPrimary Tail -->
+        
+        <!-- Banner-Ad -->
+        <section class="banner-ad">
+            <div class="ad">
+                <p>AD</p>
+            </div>
+        </section>
+
+        <!-- containerSecondary Head -->
+        <section class="secSecondary">
+        <div class="containerSecondary">
+        <div class="secRowSecondary">
+        <div class="secBorderSecondary"></div>
+            <div class="secColumnLeftSecondary">
+                <h1 class="secCategorySecondary">Editor's Picks</h1>
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a fref="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+            </div>
+        <div class="secBorderSecondary"></div>
+            <div class="secColumnMiddleSecondary">
+                <h1 class="secCategorySecondary">Trending</h1>
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+            </div>
+        <div class="secBorderSecondary"></div>
+            <div class="secColumnRightSecondary">
+                <h1 class="secCategorySecondary">User Favorites</h1>   
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+                <article>
+                    <div class="thumbnailSecondary">
+                        <img src="" width="120" height="100">
+                    </div>
+                    <div class="textSecondary">
+                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
+                        <p>(Filler Text Filler Text Filler Text)</p>                    
+                    </div>
+                </article>
+            </div>
+        <div class="secBorderSecondary"></div>
+        </div>
     </div>
-<?php include('includes/footer.html'); ?>
+    </section>
+        <!-- containerSecondary Tail -->      
+    <?php include('includes/footer.html'); ?>
+    </body>
+</html>
