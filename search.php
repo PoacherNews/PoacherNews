c9c70aa46f6c12f398de203ff3f44959cc35883e
Oruche <rro2q2@mail.missouri.edu>
2018-05-05 12:54:48 -0500

    Couple more fixes in search

--- search.php
+++ search.php
@@ -17,149 +17,150 @@
         include 'includes/header.php';
         include 'includes/nav.php';
     ?>
-    <div class="flexRow">
-        <div class="searchFlex">
-            <h1>Search Results</h1>
-            <form class="search" action="" method="GET">
-                <input type="text" id="searchInput" placeholder="What are you looking for?" name="query"/>
-                <div class="buttonFlex">
-                    <button type="submit"><i class="fa fa-search"></i></button>
+    <div class="mainFlex">
+        <div class="flexRow">
+            <div class="searchFlex">
+                <h1>Search Results</h1>
+                <form class="search" action="" method="GET">
+                    <input type="text" id="searchInput" placeholder="What are you looking for?" name="query"/>
+                    <div class="buttonFlex">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="dropdown">
+                <div class="dropbtn">Sort by 
+                    <i class="fa fa-caret-down"></i>
                 </div>
-            </form>
+                <div class="dropdown-content">
+                    <a href="search.php?sort=Relevancy">Relevancy</a>
+                    <a href="search.php?sort=Name">Name</a>
+                    <a href="search.php?sort=Views">Views</a>
+                </div>
+            </div> 
+
         </div>
-        <div class="dropdown">
-            <div class="dropbtn">Sort by 
-                <i class="fa fa-caret-down"></i>
-            </div>
-            <div class="dropdown-content">
-                <a href="search.php?sort=Relevancy">Relevancy</a>
-                <a href="search.php?sort=Name">Name</a>
-                <a href="search.php?sort=Views">Views</a>
-            </div>
-        </div> 
-        
-    </div>
-    
-    <div class="resultFlex">
-            <div class="testFlex">
-            <?php
-                $query = $_GET['query'];
-                $sort = $_GET['sort'];
-                $min_length = 1;
-                $newResults = array();
-                if(strlen($query) >= $min_length){ 
-                    $query = htmlspecialchars($query); 
-                    $query = mysql_real_escape_string($query);
-                    $raw_results = mysql_query("SELECT * FROM Articles
-                    WHERE (`Headline` LIKE '%".$query."%') OR (`Img` LIKE '%".$query."%')") or  die(mysql_error());
-                    echo "<div id='divTest'>";
-                    if(mysql_num_rows($raw_results) > 0){ 
-                        while($results = mysql_fetch_array($raw_results)){
-                            print   "<div class='flexRow'>
-                                        <div class='imgFlex'>
-                                            <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                <img src=".$results['Img']." class='image' height='120' width='140'>
-                                            </a>
+
+        <div class="resultFlex">
+                <div class="testFlex">
+                <?php
+                    $query = $_GET['query'];
+                    $sort = $_GET['sort'];
+                    $min_length = 1;
+                    if(strlen($query) >= $min_length){ 
+                        $query = htmlspecialchars($query); 
+                        $query = mysql_real_escape_string($query);
+                        $raw_results = mysql_query("SELECT * FROM Articles
+                        WHERE IsPublished = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Img` LIKE '%".$query."%'))") or die(mysql_error());
+                        echo "<div id='divTest'>";
+                        
+                        if(mysql_num_rows($raw_results) > 0){ 
+                            while($results = mysql_fetch_array($raw_results)){
+                                print   "<div class='flexRow'>
+                                            <div class='imgFlex'>
+                                                <a href=\"article.php?articleid={$results['ArticleID']}\">
+                                                    <img src=".$results['Img']." class='image' height='120' width='140'>
+                                                </a>
+                                            </div>
+                                            <div class='spanFlex'>
+                                                <span class='tip'>
+                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+                                                </span>
+                                            </div>
                                         </div>
-                                        <div class='spanFlex'>
-                                            <span class='tip'>
-                                                <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <hr class='searchHr'>";   
+                                        <hr class='searchHr'>";
+                            }
+                        }
+                        else{
+                            print "<b>No results.</b>";
                         }
                     }
-                    else{
-                        print "<b>No results.</b>";
+                    echo "</div>";
+                    // TESTING THE SORT FUNCTION
+                    if($query === is_null) {
+                        echo "Nothing to be sorted...";
+                    } else {
+                        switch($sort) {
+                            case "Relevancy":
+                                $raw_results = mysql_query("SELECT * FROM Articles ORDER BY PublishDate DESC") or die(mysql_error());
+                                echo "<div id='divTest'>";
+                                if(mysql_num_rows($raw_results) > 0){ 
+                                    while($results = mysql_fetch_array($raw_results)){
+                                        print   "<div class='flexRow'>
+                                                    <div class='imgFlex'>
+                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">
+                                                            <img src=".$results['Img']." class='image' height='120' width='140'>
+                                                        </a>
+                                                    </div>
+                                                    <div class='spanFlex'>
+                                                        <span class='tip'>
+                                                            <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <hr class='searchHr'>";
+                                    }
+                                }
+                                else{
+                                    print "<b>No results.</b>";
+                                }
+
+                                break;
+
+                            case "Name":
+                                $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Headline DESC") or die(mysql_error());
+                                echo "<div id='divTest'>";
+                                if(mysql_num_rows($raw_results) > 0){ 
+                                    while($results = mysql_fetch_array($raw_results)){
+                                        print   "<div class='flexRow'>
+                                                    <div class='imgFlex'>
+                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">
+                                                            <img src=".$results['Img']." class='image' height='120' width='140'>
+                                                        </a>
+                                                    </div>
+                                                    <div class='spanFlex'>
+                                                        <span class='tip'>
+                                                            <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <hr class='searchHr'>";
+                                    }
+                                }
+                                else{
+                                    print "<b>No results.</b>";
+                                }
+                                break;
+
+                            case "Views":
+                                $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Views DESC") or die(mysql_error());
+                                echo "<div id='divTest'>";
+                                if(mysql_num_rows($raw_results) > 0){ 
+                                    while($results = mysql_fetch_array($raw_results)){
+                                        print   "<div class='flexRow'>
+                                                    <div class='imgFlex'>
+                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">
+                                                            <img src=".$results['Img']." class='image' height='120' width='140'>
+                                                        </a>
+                                                    </div>
+                                                    <div class='spanFlex'>
+                                                        <span class='tip'>
+                                                            <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <hr class='searchHr'>";
+                                    }
+                                }
+                                else{
+                                    print "<b>No results.</b>";
+                                }
+                                break;
+                        }
                     }
-                    
-                }
-                echo "</div>";
-                // TESTING THE SORT FUNCTION
-                if($query === is_null) {
-                    echo "Nothing to be sorted...";
-                } else {
-                    switch($sort) {
-                        case "Relevancy":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY PublishDate DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            
-                            break;
-                            
-                        case "Name":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Headline DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            break;
-                            
-                        case "Views":
-                            $raw_results = mysql_query("SELECT * FROM Articles ORDER BY Views DESC") or die(mysql_error());
-                            echo "<div id='divTest'>";
-                            if(mysql_num_rows($raw_results) > 0){ 
-                                while($results = mysql_fetch_array($raw_results)){
-                                    print   "<div class='flexRow'>
-                                                <div class='imgFlex'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                        <img src=".$results['Img']." class='image' height='120' width='140'>
-                                                    </a>
-                                                </div>
-                                                <div class='spanFlex'>
-                                                    <span class='tip'>
-                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <hr class='searchHr'>";
-                                }
-                            }
-                            else{
-                                print "<b>No results.</b>";
-                            }
-                            break;
-                    }
-                }
-            ?>
-            </div>
+                ?>
+                </div>
+        </div>
     </div>
     <?php include('includes/footer.html'); ?>
 </body>
