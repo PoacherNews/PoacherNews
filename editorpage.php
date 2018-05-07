eaa9e2db3a10f8cec2b0a7838155bdd255a3b678
mgbz89 <matthewbarber1203@gmail.com>
2018-05-06 19:14:12 -0500

    Edited editor page

--- editorpage.php
+++ editorpage.php
@@ -1,48 +1,58 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
     <head>
-	<?php include 'includes/globalHead.html' ?>
+	    <?php include 'includes/globalHead.html'?>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     </head>
     <body>
         <?php
+<<<<<<< HEAD
+            //include 'loginCheck.php';
+=======
         include 'util/loginCheck.php';
+>>>>>>> 78ce0bf8bdcf09c95cca2e252ff4806db4596be5
             include 'includes/header.php';
             include 'includes/nav.php';
-            //include 'includes/footer.html';
         ?>
     <div class="epWrapper">
 
-        <form id="epEditor">
+        <form id="epEditor" action="submitArticle.php" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="action" value="saveArticle">
             <div>
                 <h1>Article Details</h1>
-                <input type="text" placeholder="Article Title" id="epArticleTitle">
-                <input type="text" placeholder="Image path" id="img">
-                <select name="category">
-                    <option selected>Category</option>
-                    <option value="politics">Politics</option>
-                    <option value="sports">Sports</option>
-                    <option value="entertainment">Entertainment</option>
-                    <option value="video">Video</option>
-                    <option value="local">Local</option>
-                    <option value="opinion">Opinion</option>
-                </select>
+                <input type="text" placeholder="Article Title" id="epArticleTitle" name="title">
+                <input list="category" name="category">
+                <datalist id="category">
+                    <option value="politics">
+                    <option value="sports">
+                    <option value="entertainment">
+                    <option value="video">
+                    <option value="local">
+                    <option value="opinion">
+                </datalist>
             </div>
             <div id="epArticle-body">
                 <h1>Article Contents</h1>
-                <textarea id="epArticleBody" placeholder="Article contents.."></textarea>
+                <textarea id="epArticleBody" placeholder="Article contents.." name="body"></textarea>
             </div>
 
             <div id="epButtons">
                 <div class="epButton-column">
-                    <input id="epImageUploadButton" type="button" value="Upload Image">
-                    <input id="epUploadTextButton" type="button" value="Upload Text File">
+                    <p>Choose Image</p>
+                    <input id="epImageUploadButton" type="file" value="Upload Image" name="image">
+                    <p>Choose Text File</p>
+                    <input id="epUploadTextButton" type="file" name="file" value="Upload Text File">
                 </div>
                 <div class="epButton-column">
-                    <input id ="epPreviewButton" type="button" value="Preview">
-                    <input id="epSubmit" type="button" value="Submit">
+                    <input id="epPreviewButton" type="submit" value="Preview">
+                    <input id="epSubmit" type="submit" value="Submit" name="submit">
                 </div>
             </div>
         </form>
+    </div>
+    <div id="preview">
     </div>
     </body>
 </html>
