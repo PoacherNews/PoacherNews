ad515305110b5e6863db9a64ecdb92835b46b1a4
Bruce Y P <bypxtd@mail.missouri.edu>
2018-05-08 12:46:29 -0500

    nav.php update

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,10 @@
     <ul class="navUl">
         <div class="navWrapper">
-            <li class="navLi"><a class="navA" href="section.php">Politics</a></li>
-            <li class="navLi" ><a class="navA" href="section.php">Sports</a></li>
-            <li class="navLi" ><a class="navA" href="section.php">Entertainment</a></li>
-            <li class="navLi" ><a class="navA" href="section.php">Video</a></li>
-            <li class="navLi" ><a class="navA" href="section.php">Local</a></li>
-            <li  class="navLi" ><a class="navA" href="section.php">Opinion</a></li>
+            <li class="navLi"><a class="navA" href="/section.php?Category=Politics">Politics</a></li>
+            <li class="navLi" ><a class="navA" href="/section.php?Category=Sports">Sports</a></li>
+            <li class="navLi" ><a class="navA" href="/section.php?Category=Entertainment">Entertainment</a></li>
+            <li class="navLi" ><a class="navA" href="/section.php?Category=Video">Video</a></li>
+            <li class="navLi" ><a class="navA" href="/section.php?Category=Local">Local</a></li>
+            <li  class="navLi" ><a class="navA" href="/section.php?Category=Opinion">Opinion</a></li>
         </div>
     </ul>
