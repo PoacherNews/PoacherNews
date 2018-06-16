4170dc4d19eaff6ae739a539a76cc8ebe80d321d
Colin Stevens <colin@colinjstevens.com>
2018-06-16 12:58:42 -0500

    Initial rework. Added editor picks and trending functionality

--- section.php
+++ section.php
@@ -1,93 +1,132 @@
-<?php session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
- ?>
+<?php 
+    session_start();
+    $defaultCategory = "Politics";
+    if(empty($_GET['Category'])) {
+        header('Location: section.php?Category='.$defaultCategory);
+        exit();
+    }
+    include('util/db.php');
+    include('util/articleUtils.php');
+?>
 <!-- TODO: 
 Fix css (Resizing issues / min-width)
 -->
 <!DOCTYPE html>
 <html>
-    <head>
-	   <?php include 'includes/globalHead.html' ?>
-    </head>
+<head>
+   <?php include 'includes/globalHead.html' ?>
+   <link rel="stylesheet" href="res/css/section.css">
+   <script>
+        function createStackedArticle(rowData) {
+            /* Makes an article DOM object and populates elements for page display.
+               Accepts a JSON formatted string object for parsing.
+            */                
+            if($.isArray(rowData)) { // In case this is an encapsulated JSON object
+                rowData = rowData[0];
+            }
+            if(rowData.hasOwnProperty('errorId')) {
+                logError(rowData);
+                return;
+            }
+            var $previewCharLimit = 400;
+            
+            var $continueReading = $("<a/>", {
+                'class' : "continue-reading",
+                'href' : "article.php?articleid="+rowData['ArticleID'],
+                'text' : "Continue Reading"
+            });
+            var $article = $("<article/>");
+            $article.append($("<div/>", {
+                'class' : "publishDate hidden",
+                'text': rowData['PublishDate']
+            }));
+            var $thumbnail = $("<div/>", {
+                'class' : "stacked-thumbnail"
+            });
+            var $text = $("<div/>", {
+                'class' : 'stacked-text'
+            });
+            
+            $thumbnail.append($("<img/>", {
+                'src' : rowData['Image'],
+                'height' : "217",
+                'width' : "325",
+            }));
+            var $headerWrap = $("<a/>", {
+                'href' : "article.php?articleid="+rowData['ArticleID']
+            });
+            $headerWrap.append($("<h1/>", {
+                'text' : rowData['Headline']
+            }));
+            $text.append($headerWrap);
+            $text.append($("<p/>", {
+                // Trim article contents to a set length, add ellipsis to denote continuation in article page
+                'text' : rowData['Body'].substring(0, $previewCharLimit)+"..."
+            }));
+            $text.append($continueReading);
+            $article.append([$thumbnail, $text]);
+            
+            return $article;
+        }
+    </script>
+</head>
     
-    <body>
-        <?php 
-            include 'includes/header.php';
-            include 'includes/nav.php';
-        ?>
-        
-        <p id="secName">Section</p>
+<body>
+    <?php 
+        include 'includes/header.php';
+        include 'includes/nav.php';
+    ?>
+    
+    <p id="secName"><?php echo $_GET['Category'] ?></p>
 
-        <!-- containerPrimary Head -->
-        <section class="secPrimary">
-        <div class="secContainerPrimary">
-        <div class="secRowPrimary">
-            <div class="secBorderPrimary"></div>
-
-            <div class="secColumnPrimary">
-<?php
-include 'util/db.php';
-$query = "SELECT * FROM Article WHERE Category='" . mysqli_real_escape_string($db, $_GET['Category']) . "' ORDER BY PublishDate DESC LIMIT 3";
-$results = mysqli_query($db, $query) or die (mysqli_error());
-
-while($row = mysqli_fetch_assoc($results)){
-if($row['IsSubmitted'] == 1) {
-$substr_value = substr($row['Body'],0,200).'...';
-          echo "<article>
-                <div class='thumbnailPrimary'>
-                    <img src='{$row['Image']}' width='350' height='250'>
-                </div>
-                <div class='textPrimary'>
-                    <h1 class='secHeadlinePrimary'><a href='/article.php?articleid={$row['ArticleID']}'>{$row['Headline']}</a></h1>
-                    <p>$substr_value</p>
-                </div>
-            </article>";
-}
-}
-?>
-
-<!--      
-            <article>
-                <div class="thumbnailPrimary">
-                    <img src="" width="350" height="250">
-                </div>
-                <div class="textPrimary">
-                    <h1 class="secHeadlinePrimary"><a href="">(Headline Placeholder)</a></h1>
-                    <p>(Filler Text Filler Text Filler Text)</p>
-                </div>
-            </article>    
-                
-            <article>
-                <div class="thumbnailPrimary">
-                    <img src="" width="350" height="250">
-                </div>
-                <div class="textPrimary">
-                    <h1 class="secHeadlinePrimary"><a href="">(Headline Placeholder)</a></h1>
-                    <p>(Filler Text Filler Text Filler Text)</p>
-                </div>
-            </article>
--->
-
-            <!-- Show More -->
-            <div class="showMore">
-                <a href="">Show More</a>
+    <!-- containerPrimary Head -->
+    <section class="secPrimary">
+    <div class="secContainerPrimary">
+    <div class="secRowPrimary">
+        <div class="secBorderPrimary"></div>
+        <div class="secColumnPrimary">
+            <div id="articleList">
+            <script>
+                $.getJSON("util/sectionUtil.php", {
+                    'Category' : "<?php echo $_GET['Category'] ?>",
+                }).done(function(data) {
+                    $.each(data, function(i, row) {
+                        // $("#secondary-section").children().remove('.loader');
+                        $("#articleList").append(createStackedArticle(row));
+                    });
+                });
+            </script>
             </div>
-                
-            </div>
-            <div class="secBorderPrimary"></div>
+            <div class="showMore">Show More</div>
+            <script>
+                function getLastPublishDate() {
+                    var $lastArticle = $("#articleList").children("article");
+                    return $lastArticle.children(".publishDate").text();
+                };
+                $(".showMore").click(function() {
+                    console.log("Clicked"); //DEBUG
+                    getLastPublishDate();
+                    $.getJSON("util/sectionUtil.php", {
+                        'offset' : getLastPublishDate(),
+                        'Category' : "<?php echo $_GET['Category'] ?>",
+                    }).done(function(data) {
+                        $.each(data, function(i, row) {
+                            // $("#secondary-section").children().remove('.loader');
+                            $("#articleList").append(createStackedArticle(row));
+                        });
+                    });
+                });
+            </script>
+            <!-- <div class="loader"></div> -->
         </div>
-        </div>
-        </section>
+
+        <div class="secBorderPrimary"></div>
+    </div>
+    </div>
+    </section>
         <!-- containerPrimary Tail -->
         
-        <!-- Banner-Ad -->
-        <section class="banner-ad">
-            <div class="ad">
-                <p>AD</p>
-            </div>
-        </section>
+
 
         <!-- containerSecondary Head -->
         <section class="secSecondary">
@@ -96,46 +135,38 @@ $substr_value = substr($row['Body'],0,200).'...';
         <div class="secBorderSecondary"></div>
             <div class="secColumnLeftSecondary">
                 <h1 class="secCategorySecondary">Editor's Picks</h1>
-                <article>
-                    <div class="thumbnailSecondary">
-                        <img src="" width="120" height="100">
-                    </div>
-                    <div class="textSecondary">
-                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
-                        <p>(Filler Text Filler Text Filler Text)</p>                    
-                    </div>
-                </article>
-                <article>
-                    <div class="thumbnailSecondary">
-                        <img src="" width="120" height="100">
-                    </div>
-                    <div class="textSecondary">
-                        <h2 class="secHeadlineSecondary"><a fref="">(Headline Placeholder)</a></h2>
-                        <p>(Filler Text Filler Text Filler Text)</p>                    
-                    </div>
-                </article>
-            </div>
+                <?php
+                    $picks = getEditorPicks($db);
+                    foreach($picks as $article) {
+                        print "<article>
+                            <div class=\"thumbnailSecondary\">
+                                <a href=\"article.php?articleid={$article['ArticleID']}\"><img src=\"{$article['Image']}\" width=\"150\" height=\"100\"></a>
+                            </div>
+                            <div class=\"textSecondary\">
+                                <h2 class=\"secHeadlineSecondary\"><a href=\"article.php?articleid={$article['ArticleID']}\">{$article['Headline']}</a></h2>
+                                <p>".substr($article['Body'], 0, 75)."...</p>                    
+                            </div>
+                        </article>";
+                    }
+                ?>
+              </div>
         <div class="secBorderSecondary"></div>
             <div class="secColumnMiddleSecondary">
                 <h1 class="secCategorySecondary">Trending</h1>
-                <article>
-                    <div class="thumbnailSecondary">
-                        <img src="" width="120" height="100">
-                    </div>
-                    <div class="textSecondary">
-                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
-                        <p>(Filler Text Filler Text Filler Text)</p>                    
-                    </div>
-                </article>
-                <article>
-                    <div class="thumbnailSecondary">
-                        <img src="" width="120" height="100">
-                    </div>
-                    <div class="textSecondary">
-                        <h2 class="secHeadlineSecondary"><a href="">(Headline Placeholder)</a></h2>
-                        <p>(Filler Text Filler Text Filler Text)</p>                    
-                    </div>
-                </article>
+                <?php
+                    $trending = getTrendingArticles(3, $db);
+                    foreach($trending as $article) {
+                        print "<article>
+                            <div class=\"thumbnailSecondary\">
+                                <a href=\"article.php?articleid={$article['ArticleID']}\"><img src=\"{$article['Image']}\" width=\"150\" height=\"100\"></a>
+                            </div>
+                            <div class=\"textSecondary\">
+                                <h2 class=\"secHeadlineSecondary\"><a href=\"article.php?articleid={$article['ArticleID']}\">{$article['Headline']}</a></h2>
+                                <p>".substr($article['Body'], 0, 75)."...</p>
+                            </div>
+                        </article>";
+                    }
+                ?>
             </div>
         <div class="secBorderSecondary"></div>
             <div class="secColumnRightSecondary">
