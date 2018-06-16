99479a3af1195500509f2fb8c4d7ed95be2cc06f
Roland Oruche <rolandoruche@Harriets-MacBook-Pro-2.local>
2018-06-15 23:30:46 -0500

    fixed sorting search

--- search.php
+++ search.php
@@ -1,3 +1,7 @@
+<?php
+	include 'util/loginCheck.php';
+?>
+
 <!DOCTYPE>
 <html>
 <head>
@@ -12,7 +16,6 @@
 </head>
 <body>
     <?php
-		include 'util/loginCheck.php';
         include 'includes/header.php';
         include 'includes/nav.php';
     ?>
@@ -21,7 +24,9 @@
             <div class="searchFlex">
                 <h1>Search Results</h1>
                 <form class="search" action="" method="GET">
-                    <input type="text" id="searchInput" placeholder="What are you looking for?" name="query"/>
+                    <?php
+                        print "<input type=\"text\" id=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query\" value={$_GET['query']}>";
+                    ?>
                     <div class="buttonFlex">
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </div>
@@ -32,9 +37,11 @@
                     <i class="fa fa-caret-down"></i>
                 </div>
                 <div class="dropdown-content">
-                    <a href="search.php?sort=Relevancy">Relevancy</a>
-                    <a href="search.php?sort=Name">Name</a>
-                    <a href="search.php?sort=Views">Views</a>
+				<?php
+                    print "<a href=\"search.php?query={$_GET['query']}&sort=Relevancy\">Relevancy</a>";
+                    print "<a href=\"search.php?query={$_GET['query']}&sort=Name\">Name</a>";
+                    print "<a href=\"search.php?query={$_GET['query']}&sort=Views\">Views</a>";
+				?>
                 </div>
             </div> 
 
@@ -46,46 +53,46 @@
                     $query = $_GET['query'];
                     $sort = $_GET['sort'];
                     $min_length = 1;
-                    if(strlen($query) >= $min_length){ 
-                        $query = htmlspecialchars($query); 
-                        $query = mysqli_real_escape_string($db, $query);
-                        $raw_results = "SELECT * FROM Article
-                        WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%'))";
-                        $test = mysqli_query($db, $raw_results);
-                        echo "<div id='divTest'>";
-                        
-                        if(mysqli_num_rows($test) > 0){ 
-                            while($results = mysqli_fetch_array($test, MYSQLI_ASSOC)){
-                                print   "<div class='flexRow'>
-                                            <div class='imgFlex'>
-                                                <a href=\"article.php?articleid={$results['ArticleID']}\">
-                                                    <img src=".$results['Image']." class='image' height='120' width='140'>
-                                                </a>
-                                            </div>
-                                            <div class='spanFlex'>
-                                                <span class='tip'>
-                                                    <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <hr class='searchHr'>";
-                            }
-                        }
-                        else{
-                            print "<b>No results.</b>";
-                        }
-                    }
-                    echo "</div>";
+					if(!(isset($_GET['sort']))) {
+						if(strlen($query) >= $min_length){ 
+							$query = htmlspecialchars($query); 
+							$query = mysqli_real_escape_string($db, $query);
+
+							$raw_results = "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%'))";
+							$test = mysqli_query($db, $raw_results);
+							echo "<div class='search-content'>";
+
+							if(mysqli_num_rows($test) > 0){ 
+								while($results = mysqli_fetch_array($test, MYSQLI_ASSOC)){
+									print   "<div class='flexRow'>
+												<div class='imgFlex'>
+													<a href=\"article.php?articleid={$results['ArticleID']}\">
+														<img src=".$results['Image']." class='image' height='120' width='140'>
+													</a>
+												</div>
+												<div class='spanFlex'>
+													<span class='tip'>
+														<a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+													</span>
+												</div>
+											</div>
+											<hr class='searchHr'>";
+								}
+							}
+							else{
+								print "<b>No results.</b>";
+							}
+							echo "</div>";
+						}
+                    
                     // TESTING THE SORT FUNCTION
-                    if($query === is_null) {
-                        echo "Nothing to be sorted...";
                     } else {
                         switch($sort) {
                             case "Relevancy":
-                                $raw_results = mysql_query("SELECT * FROM Article ORDER BY PublishDate DESC") or die(mysql_error());
-                                echo "<div id='divTest'>";
-                                if(mysql_num_rows($raw_results) > 0){ 
-                                    while($results = mysql_fetch_array($raw_results)){
+                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY PublishDate DESC") or die(mysql_error());
+                                echo "<div class='search-content'>";
+                                if(mysqli_num_rows($raw_results) > 0){ 
+                                    while($results = mysqli_fetch_array($raw_results)){
                                         print   "<div class='flexRow'>
                                                     <div class='imgFlex'>
                                                         <a href=\"article.php?articleid={$results['ArticleID']}\">
@@ -108,10 +115,10 @@
                                 break;
 
                             case "Name":
-                                $raw_results = mysql_query("SELECT * FROM Article ORDER BY Headline DESC") or die(mysql_error());
-                                echo "<div id='divTest'>";
-                                if(mysql_num_rows($raw_results) > 0){ 
-                                    while($results = mysql_fetch_array($raw_results)){
+                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Headline ASC") or die(mysql_error());
+                                echo "<div class='search-content'>";
+                                if(mysqli_num_rows($raw_results) > 0){ 
+                                    while($results = mysqli_fetch_array($raw_results)){
                                         print   "<div class='flexRow'>
                                                     <div class='imgFlex'>
                                                         <a href=\"article.php?articleid={$results['ArticleID']}\">
@@ -133,10 +140,10 @@
                                 break;
 
                             case "Views":
-                                $raw_results = mysql_query("SELECT * FROM Article ORDER BY Views DESC") or die(mysql_error());
-                                echo "<div id='divTest'>";
-                                if(mysql_num_rows($raw_results) > 0){ 
-                                    while($results = mysql_fetch_array($raw_results)){
+                               $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Views DESC") or die(mysql_error());
+                                echo "<div class='search-content'>";
+                                if(mysqli_num_rows($raw_results) > 0){ 
+                                    while($results = mysqli_fetch_array($raw_results)){
                                         print   "<div class='flexRow'>
                                                     <div class='imgFlex'>
                                                         <a href=\"article.php?articleid={$results['ArticleID']}\">
@@ -152,12 +159,12 @@
                                                 <hr class='searchHr'>";
                                     }
                                 }
-                                else{
+                                else {
                                     print "<b>No results.</b>";
                                 }
                                 break;
-                        }
-                    }
+                        }//End of switch
+                    }//End of else
                 ?>
                 </div>
         </div>
