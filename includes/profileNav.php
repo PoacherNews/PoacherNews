e0ac214ce507eb411be1b099dd8c9f6d62645606
Bruce Y P <bypxtd@mail.missouri.edu>
2018-06-17 21:02:49 -0500

    Tools removal

--- includes/profileNav.php
+++ includes/profileNav.php
@@ -52,14 +52,4 @@
     ?>
     </li>
     <?php } ?>
-
-    <?php if($_SESSION['usertype'] == 'W' || $_SESSION['usertype'] == 'A') { ?>
-    <li <?php if($current == 'editorPage') {echo 'class="current"';} ?>><a href="/editorpage.php">Editor Page</a></li>
-    <?php } ?>
-    
-    <!-- Admin Tools -->
-    <?php if($_SESSION['usertype'] == 'A') { ?>
-    <li <?php if($current == 'manageUsers'){echo 'class="current"';} ?>><a href="/userManagement.php">Manage Users</a></li>
-    <li <?php if($current == 'manageArticles'){echo 'class="current"';} ?>><a href="/articleManagement.php">Manage Articles</a></li>
-    <?php } ?>
 </ul>
