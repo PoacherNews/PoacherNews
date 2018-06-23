254cf49e3036b80192c97c6878acff6d1930409c
Roland Oruche <rolandoruche@mwc-014234.dhcp.missouri.edu>
2018-06-23 14:22:00 -0500

    New rich text editor

--- search.php
+++ search.php
@@ -58,7 +58,7 @@
 							$query = htmlspecialchars($query); 
 							$query = mysqli_real_escape_string($db, $query);
 
-							$raw_results = "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%'))";
+							$raw_results = "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%'))";
 							$test = mysqli_query($db, $raw_results);
 							echo "<div class='search-content'>";
 
@@ -89,7 +89,7 @@
                     } else {
                         switch($sort) {
                             case "Relevancy":
-                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY PublishDate DESC") or die(mysql_error());
+                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY PublishDate DESC") or die(mysql_error());
                                 echo "<div class='search-content'>";
                                 if(mysqli_num_rows($raw_results) > 0){ 
                                     while($results = mysqli_fetch_array($raw_results)){
@@ -115,7 +115,7 @@
                                 break;
 
                             case "Name":
-                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Headline ASC") or die(mysql_error());
+                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Headline ASC") or die(mysql_error());
                                 echo "<div class='search-content'>";
                                 if(mysqli_num_rows($raw_results) > 0){ 
                                     while($results = mysqli_fetch_array($raw_results)){
@@ -140,7 +140,7 @@
                                 break;
 
                             case "Views":
-                               $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Views DESC") or die(mysql_error());
+                               $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`Image` LIKE '%".$query."%')) ORDER BY Views DESC") or die(mysql_error());
                                 echo "<div class='search-content'>";
                                 if(mysqli_num_rows($raw_results) > 0){ 
                                     while($results = mysqli_fetch_array($raw_results)){
