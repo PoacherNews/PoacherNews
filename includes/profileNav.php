52a1cf5d34567fbce0d663b45fdc0a2fbf6b8bea
Bruce Y P <bypxtd@mail.missouri.edu>
2018-06-27 13:39:43 -0500

    Redirect from ID fix

--- includes/profileNav.php
+++ includes/profileNav.php
@@ -1,21 +1,21 @@
 <ul>
     <li <?php if($current == 'overview') {echo 'class="current"';} ?>>
     <?php
-    echo "<a href='/profile.php?Username=".$username."'>";
+    echo "<a href='/profile.php?uid=".$userid."'>";
     echo "General</a>";
     ?>
     </li>
     
     <li <?php if($current == 'comments') {echo 'class="current"';} ?>>
     <?php
-    echo "<a href='/comments.php?Username=".$username."'>";
+    echo "<a href='/comments.php?uid=".$userid."'>";
     echo "Comments</a>";
     ?>
     </li>
     
     <li <?php if($current == 'favorites'){echo 'class="current"';} ?>>
     <?php
-    echo "<a href='/favorites.php?Username=".$username."'>";
+    echo "<a href='/favorites.php?uid=".$userid."'>";
     echo "Favorites</a>";
     ?>
     </li>
@@ -24,7 +24,7 @@
     <?php if($usertype == 'W' || $usertype == 'A') { ?>
     <li <?php if($current == 'editorHistoryProfile') {echo 'class="current"';} ?>>
     <?php
-    echo "<a href='/editorHistory.php?Username=".$username."'>";
+    echo "<a href='/editorHistory.php?uid=".$userid."'>";
     echo "Editor History</a>";
     ?>
     </li>
