a24e4a911a6889e622f16ebf7a932f4066292101
Bruce Y P <bypxtd@mail.missouri.edu>
2018-06-27 12:59:39 -0500

    Prepared statement utilization

--- util/handleBio.php
+++ util/handleBio.php
@@ -1,18 +1,50 @@
 <?php
-session_start();
+    session_start();   
 
+    // connection to database
     include 'db.php';
+    // Check connection
+    if ($db->connect_error)
+    {
+       die("Connection failed: " . $db->connect_error);
+    }
 
+    // set $bio var to ajax data text
     $bio =$_POST['text'];
     //echo $test;
-    $sql="UPDATE User SET Bio = '".$bio."' WHERE Username = '".$_SESSION['username']."'"; 
-    //echo $sql;
-   
-if ($db->query($sql) === FALSE) {
-    echo "ERROR";
-}            
-    
-    //set session
-    $_SESSION['Bio'] = $bio;
+    //$query="UPDATE User SET Bio = '".$bio."' WHERE UserID = '".$_SESSION['userid']."'"; 
 
+    // build new statement to insert new bio in Users table
+    $stmt = $db->stmt_init();
+    // prepare
+    if (!$stmt->prepare("UPDATE User SET Bio = '".$bio."' WHERE UserID = ?"))
+    {
+        echo "Error preparing INSERT statement: \n";
+        echo nl2br(print_r($stmt->error_list, true), false);
+        exit;
+    }
+    // bind parameters to new statement
+    if (!$stmt->bind_param('i', $_SESSION['userid']))
+    {
+        echo "Error binding parameters to INSERT: \n";
+        echo nl2br(print_r($stmt->error_list, true), false);
+        exit;
+    }
+    //print_r($result);
+
+    // execute statement
+    if (!$stmt->execute())
+    {
+        echo "Error INSERTing: \n";
+        echo nl2br(print_r($stmt->error_list, true), false);
+        exit;
+    }
+
+    $stmt->close();
+    $db->close();
+    
+    // $_SESSION UPDATE
+    $_SESSION['Bio'] = $bio;  
 ?>
+
+
