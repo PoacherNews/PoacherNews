efba78c38ee110737461227d61e9b9aefc30efc5
Bruce Y P <bypxtd@mail.missouri.edu>
2018-06-27 12:58:50 -0500

    Bio functionality update / code formatting

--- includes/profileHeader.php
+++ includes/profileHeader.php
@@ -1,7 +1,9 @@
 <?php
 // TODO:
 // Error check for blank bio
-// Change profile picture
+// On cancel set textarea to biocontainer text
+// 
+// Change / Remove profile picture
     include 'util/db.php';
     include 'util/userUtils.php';
 
@@ -48,6 +50,10 @@
 	width: 70px;
 }
 
+.bioContainer {
+    display: none;
+}
+    
 #text_id {
 	width: 200px;
 	height: 70px;
@@ -57,145 +63,142 @@
 
 <div class="user">
     <div class="picture">
-<?php
-    if($profilepicture != null)
-    {
-        echo "<img src='../res/img/profilePictures/".$username."/".$profilepicture."'>";
-    }
-    else 
-    {
-        echo "(Profile Picture)";
-        if(strtolower($username) == strtolower($_SESSION['username']))
+    <?php
+        if($profilepicture != null)
         {
-?>
-            <form action="../util/uploadProfilePicture.php" method="post" enctype="multipart/form-data">
-                Select image to upload:
-                <input type="file" name="profilePicture" class="profilePicture">
-                <input type="submit" value="Upload Image" name="submit">
-            </form>
-<?php
+            echo "<img src='../res/img/profilePictures/".$username."/".$profilepicture."'>";
         }
-    }
-?>
+        else 
+        {
+            echo "(Profile Picture)";
+            if($userid == $_SESSION['userid'])
+            {
+    ?>
+                <form action="../util/uploadProfilePicture.php" method="post" enctype="multipart/form-data">
+                    Select image to upload:
+                    <input type="file" name="profilePicture" class="profilePicture">
+                    <input type="submit" value="Upload Image" name="submit">
+                </form>
+        <?php
+            }
+        }
+        ?>
     </div>
             
     <div class="info">
-        <?php 
-            echo "<h3>$username</h3>";
-            if($bio != null)
+    <?php 
+        echo "<h3>$username</h3>";
+        if($bio != null)
+        {
+    ?>
+            <div class="bioContainer2">
+                <?php echo $bio; ?>
+            </div>
+                
+            <form action="../util/handleBio.php" method="POST">
+                <textarea name="text" id="text_id" style="resize:vertical;display:none"><?php echo $bio ?></textarea>
+                <button type="button" name="save" class="save_button">Save</button>
+            </form>
+                
+            <button type="button" class="edit_button2">Edit</button>
+            <button type="button" class="cancel_button">Cancel</button>
+            
+            <script>
+                $(".edit_button2").click(function(){$(".bioContainer2").toggle(), $("#text_id").toggle(), $(".edit_button2").hide(), $(".save_button").show(), $(".cancel_button").show()})
+                $(".cancel_button").click(function(){$(".bioContainer2").toggle(), $("#text_id").toggle(), $(".edit_button2").show(), $(".save_button").hide(), $(".cancel_button").hide()})
+        
+                $(document).ready(function()
+                {
+                    $(".save_button").click(function()
+                    {
+                        $(".bioContainer2").toggle(),
+                        $("#text_id").toggle(),
+                        $(".edit_button2").show(), 
+                        $(".save_button").hide(), 
+                        $(".cancel_button").hide()
+	        
+                        var bio = $('textarea#text_id').val();
+                        // Test JQuery var
+                        //$(".bioContainer").text(bio);
+
+                        $.ajax({
+                            type: 'POST',
+                            url: '../util/handleBio.php',
+                            async: false,
+                            data: {text : bio},
+                            success: function (data) 
+                            {
+                                $(".bioContainer2").html(bio);
+                            },
+                            error: function () 
+                            {
+                                $(".bioContainer2").html('Error loading information from server.');
+                            }
+                        });  
+                    });
+                });
+            </script>
+        
+        <?php
+        }
+        else
+        {
+            if($userid == $_SESSION['userid'])
             {
         ?>
                 <div class="bioContainer">
-                    <?php echo $bio; ?>
                 </div>
+                    
                 <form action="../util/handleBio.php" method="POST">
-                    <textarea name="text" id="text_id" class="form-control" style="resize:vertical;display:none"><?php echo $bio ?></textarea>
+                    <textarea name="text" id="text_id" class="form-control" style="resize:vertical;display:none"></textarea>
                     <button type="button" name="save" class="save_button">Save</button>
                 </form>
-                
-        <button type="button" class="edit_button2">Edit</button>
-        <button type="button" class="cancel_button">Cancel</button>
-<script>
-    	$(".edit_button2").click(function(){$(".bioContainer").toggle(), $("#text_id").toggle(), $(".edit_button2").hide(), $(".save_button").show(), $(".cancel_button").show()})
-        $(".cancel_button").click(function(){$(".bioContainer").toggle(), $("#text_id").toggle(), $(".edit_button2").show(), $(".save_button").hide(), $(".cancel_button").hide()})
-        
-        $(document).ready(function() {
 
-    $(".save_button").click(function()
-    {
-        $(".bioContainer").toggle(),
-        $("#text_id").toggle(),
-        $(".edit_button2").show(), 
-        $(".save_button").hide(), 
-        $(".cancel_button").hide()
+                <button type="button" class="add_button">Add Bio</button>
+                <button type="button" class="cancel_button">Cancel</button>
+                <button type="button" class="edit_button">Edit</button>
+
+                <script>
+                    $(".add_button").click(function(){$("#text_id").toggle(), $(".add_button").hide(), $(".save_button").show(), $(".cancel_button").show()})
+                    $(".cancel_button").click(function(){$("#text_id").toggle(), $(".add_button").show(), $(".save_button").hide(), $(".cancel_button").hide()})
+                    $(".edit_button").click(function(){$(".bioContainer").toggle(), $("#text_id").toggle(), $(".edit_button").hide(), $(".save_button").show(), $(".cancel_button").show()})
+
+                    $(document).ready(function()
+                    {
+                        $(".save_button").click(function()
+                        {
+                            $(".bioContainer").toggle(),
+                            $("#text_id").toggle(),
+                            $(".edit_button").show(), 
+                            $(".add_button").hide(), 
+                            $(".save_button").hide(), 
+                            $(".cancel_button").hide()
 	        
-        var bio = $('textarea#text_id').val();
-        // Test JQuery var
-        //$(".bioContainer").text(bio);
+                            var bio = $('textarea#text_id').val();
+                            // Test JQuery var
+                            //$(".bioContainer").text(bio);
 
-        $.ajax({
-            type: 'POST',
-            url: '../util/handleBio.php',
-            async: false,
-            data: {text : bio},
-            success: function (data) 
-            {
-                $(".bioContainer").html(bio);
-            },
-            error: function () 
-            {
-                $(".bioContainer").html('Error loading information from server.');
-            }
-        });  
+                            $.ajax({
+                                type: 'POST',
+                                url: '../util/handleBio.php',
+                                async: false,
+                                data: {text : bio},
+                                success: function (data) 
+                                {
+                                    $(".bioContainer").html(bio);
+                                },
+                                error: function () 
+                                {
+                                    $(".bioContainer").html('Error loading information from server.');
+                                }
+                            });  
                 
-    });
-});
-
-
-</script>
-        
-        <?php
-            }
-            else
-            {
-            	if(strtolower($username) == strtolower($_SESSION['username']))
-        		{
-        ?>
-                <div class="bioContainer">
-                </div>
-                <form action="../util/handleBio.php" method="POST">
-<textarea name="text" id="text_id" class="form-control" style="resize:vertical;display:none"></textarea>
-<button type="button" name="save" class="save_button">Save</button>
-</form>
-
-<button type="button" class="add_button">Add Bio</button>
-<button type="button" class="cancel_button">Cancel</button>
-<button type="button" class="edit_button">Edit</button>
-
-<script>
-	$(".add_button").click(function(){$("#text_id").toggle(), $(".add_button").hide(), $(".save_button").show(), $(".cancel_button").show()})
-	$(".cancel_button").click(function(){$("#text_id").toggle(), $(".add_button").show(), $(".save_button").hide(), $(".cancel_button").hide()})
-    $(".edit_button").click(function(){$(".bioContainer").toggle(), $("#text_id").toggle(), $(".edit_button").hide(), $(".save_button").show(), $(".cancel_button").show()})
-
-
-$(document).ready(function() {
-
-    $(".save_button").click(function()
-    {
-        $("#text_id").toggle(),
-        $(".edit_button").show(), 
-        $(".add_button").hide(), 
-        $(".save_button").hide(), 
-        $(".cancel_button").hide()
-	        
-        var bio = $('textarea#text_id').val();
-        // Test JQuery var
-        //$(".bioContainer").text(bio);
-
-        $.ajax({
-            type: 'POST',
-            url: '../util/handleBio.php',
-            async: false,
-            data: {text : bio},
-            success: function (data) 
-            {
-                $(".bioContainer").html(bio);
-            },
-            error: function () 
-            {
-                $(".bioContainer").html('Error loading information from server.');
-            }
-        });  
-                
-    });
-});
-
-
-</script>
-
-        <?php
-        		}
-            }
-        ?>
+                        });
+                    });
+                </script>
+    <?php
+        }
+    }
+    ?>
     </div>
 </div>
