7b152f4429bcea073624bc35d40e668c9a2fb489
Colin Stevens <colin@colinjstevens.com>
2018-07-30 16:27:00 -0500

    Restyled and updated login page

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
     <?php include 'includes/globalHead.html' ?>
+    <title>Login</title>
 </head>
 <body>
     <?php
@@ -14,35 +14,34 @@
         if(empty($_SESSION['loggedin'])) {
             $loggedIn = false;
         } else { // The user is already logged in, so send them back to the index
-            header('Location: index.php');
-            // echo '<meta http-equiv="refresh" content="0; url=/index.php">';
+             echo '<meta http-equiv="refresh" content="0; url=/index.php">';
             exit;
         }
     	include 'includes/header.php';
         include 'includes/nav.php';
     ?>
-    <div class="pageContent">
-    <form action="/util/handleLogin.php" method="POST">
-        <div class="loginWrap">
-            <div class="form">
-                <h1>Login</h1>
-                <label for="username">Username: </label>
-                <input type="text" name="username" placeholder="Username">
-                <br>
-                <label for="password">Password: </label>
-                <input type="password" name="password" placeholder="Password">
-                <div class="buttons">
-                    <input type="submit" name="submit" value="Submit">
-                    <a href="/createUser.php">Create an account</a>
+    <div id="mainContent">
+        <form action="/util/handleLogin.php" method="POST">
+            <div class="loginWrap">
+                <div class="form">
+                    <h1>Login</h1>
+                    <label for="username">Username: </label>
+                    <input type="text" name="username" placeholder="Username">
+                    <br>
+                    <label for="password">Password: </label>
+                    <input type="password" name="password" placeholder="Password">
+                    <div class="buttons">
+                        <input type="submit" name="submit" value="Submit">
+                        <a href="/createUser.php">Create an account</a>
+                    </div>
+                    <?php
+                        if (isset($error)) {
+                            echo "<p class='loginError'>$error</p>";
+                        }
+                    ?>
                 </div>
-                <?php
-                    if (isset($error)) {
-                        echo "<p>$error</p>";
-                    }
-                ?>
             </div>
-        </div>
-    </form>
+        </form>
     </div>
     <?php include('includes/footer.html'); ?>
 </body>
