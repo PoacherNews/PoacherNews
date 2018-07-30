61b150093e6cac03ba8300d886632c1bf640a234
Colin Stevens <colin@colinjstevens.com>
2018-07-30 16:03:54 -0500

    Updated all info pages to grid and added more information

--- feedback.php
+++ feedback.php
@@ -4,24 +4,24 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
 	<?php include 'includes/globalHead.html' ?>
-    </head>
-    <body>
-        <?php 
-            include 'includes/header.php';
-            include 'includes/nav.php';
-        ?>
-        <div class="fbDivLeft"></div> 
-        <div class="fbDivRight"></div>
-        <p id="fbTitleId"><b>Feedback</b></p>
-        <br>
-        <p class="fbP">Our staff here at <i>The Poacher</i> really appreciates what our users think about our content. Let us know how our website is doing! </p>
-        <p class="fbP">For more information, please contact:</p>
-        <h2 class="fbHead2">poachernews@gmail.com</h2>
-        <hr class="fbHr">
-        <div id="fbDivAds">Placeholder Advertisement</div>
-        <hr class="fbHr">
-        <?php include('includes/footer.html'); ?>
-    </body>
+    <link rel="stylesheet" type="text/css" href="res/css/feedback.css">
+</head>
+<body>
+    <?php 
+        include 'includes/header.php';
+        include 'includes/nav.php';
+    ?>
+    <div id="mainContent">
+        <h1 id="feedbackHeader">Feedback</h1>
+        <p>
+            The staff here at <span class="text-emphasis">The Poacher</span> really appreciate what our users think about our content. Let us know how our website is doing!
+        </p>
+        <p>
+            For feedback or more information, please contact:<a id="feedbackContactLink" href="mailto:poachernews@gmail.com">poachernews@gmail.com</a>
+        </p>
+    </div>
+    <?php include('includes/footer.html'); ?>
+</body>
 </html>
--- help.php
+++ help.php
@@ -4,49 +4,56 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
 	<?php include 'includes/globalHead.html' ?>
-    </head>
-    <body>
-        <?php            
-            include 'includes/header.php';
-            include 'includes/nav.php';
-        ?>
-        <div class="helpDivLeft"></div> 
-        <div class="helpDivRight"></div>
-        <p id="helpTitleId"><b>Frequently Asked Questions</b></p>
-        <br>
-        <h1 class="helpHead1">Basics:</h1>
+    <link rel="stylesheet" type="text/css" href="res/css/help.css">
+</head>
+<body>
+    <?php            
+        include 'includes/header.php';
+        include 'includes/nav.php';
+    ?>
+    <div id="mainContent">
+        <h1 id="helpHeader">Frequently Asked Questions</h1>
+        <h2 class="helpSectionHeader">Basics</h2>
         <ul> 
-            <li><h2 class="helpHead2">Q: What is the Poacher?</h2></li>
-            <li><h2 class="helpHead2">Q: What is 'satrical' news?</h2></li>
-            <li><h2 class="helpHead2">Q: How can I become a user?</h2></li>
+            <li><h3 class="questionHeader">Q: What is <span class="text-emphasis">The Poacher?<span></h2></li>
+            <li><span class="text-emphasis">The Poacher</span> is a satirical online news publication made by students, for students. It allows aspiring and accomplished authors alike a home to post articles and provides a space for a community to read, comment, and engage with one another.</li>
+            <li><h3 class="questionHeader">Q: What is 'satrical' news?</h3></li>
+            <li><h3 class="questionHeader">Q: How can I become a user?</h3></li>
+            <li>Anyone can sign up for an account free of charge <a href="createUser.php">here</a>.
         </ul>
-        <h1 class="helpHead1">Account Information:</h1>
+        <h2 class="helpSectionHeader">Account Information</h2>
         <ul>
-            <li><h2 class="helpHead2">Q: Where can I signup/login/logout?</h2></li>
-            <li><h2 class="helpHead2">Q: What capabilities do I have as a user?</h2></li>
+            <li><h3 class="questionHeader">Q: Where can I signup/login/logout?</h3></li>
+            <li>The account creation page is located <a href="createUser.php">here</a>. You can log in or log out by using the drop down menu in the upper right of the header, or by accessing the login page <a href="login.php">here</a>.
+            <li><h3 class="questionHeader">Q: What capabilities do I have as a user?</h3></li>
+            <li>Anyone can read articles published on <span class="text-emphasis">The Poacher</span>, but those with registered accounts can rate articles, bookmark articles for later reading, and leave comments.</li>
+            <li><h3 class="questionHeader">Q: What if I  want to write for <span class="text-emphasis">The Poacher</span>?</h3></li>
+            <li>To apply for writer status, please contact us via the information on our <a href="feedback.php">feedback page</a>.</li>
         </ul>
-        <h1 class="helpHead1">Terms of Use:</h1>
+        <h2 class="helpSectionHeader">Terms of Use</h2>
         <ul>
-            <li><h2 class="helpHead2">Q: What are the Terms of Use?</h2></li>
-            <li><h2 class="helpHead2">Q: Do I have to sign the Terms of Use?</h2></li>
+            <li><h3 class="questionHeader">Q: What are the Terms of Use?</h3></li>
+            <li>Our Terms of Use can be found <a href="terms.php">here</a>.
+            <li><h3 class="questionHeader">Q: Do I have to sign the Terms of Use?</h3></li>
+            <li>All new accounts must explicitly agree to the Terms of Use during the account creation process.</li>
         </ul>
-        <h1 class="helpHead1">Contact:</h1>
+        <h2 class="helpSectionHeader">Contact</h2>
         <ul>
-            <li><h2 class="helpHead2">Q: Who can I contact for Feedback/Questions/Concerns?</h2></li>
+            <li><h3 class="questionHeader">Q: Who can I contact for Feedback/Questions/Concerns?</h3></li>
+            <li>If you feel the need to contact the staff at <span class="text-emphasis">The Poacher</span> for any reason, you can find the contact information <a href="feedback.php">here</a>.</li>
         </ul>
-        <h1 class="helpHead1">Social Media:</h1>
+        <h2 class="helpSectionHeader">Social Media</h2>
         <ul>
-            <li><h2 class="helpHead2">Q: What social media platforms is <i>The Poacher</i> on ?</h2></li>
+            <li><h3 class="questionHeader">Q: What social media platforms is <span class="text-emphasis">The Poacher</span> on?</h3></li>
         </ul>
-        <h1 class="helpHead1">Advertising:</h1>
+        <h2 class="helpSectionHeader">Advertising</h2>
         <ul>
-            <li><h2 class="helpHead2">Q: Can I advertise for <i>The Poacher</i>?</h2></li>
+            <li><h3 class="questionHeader">Q: Can I advertise for <span class="text-emphasis">The Poacher</span>?</h3></li>
+            <li>Yes! For advertisement inquries, please visit our <a href="advertising.php">advertising page</a>.</li>
         </ul>
-        <hr class="helpHr">
-        <div id="helpDivAds">Placeholder Advertisement</div>
-        <hr class="helpHr">
-        <?php include('includes/footer.html'); ?>
+    </div>
+    <?php include('includes/footer.html'); ?>
     </body>
 </html>
