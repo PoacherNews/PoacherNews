b4facc4d0f9fb104bd23d6fd7dedfac8f8b627e7
Colin Stevens <colin@colinjstevens.com>
2018-08-14 21:36:09 -0500

    Reattempted mobile restyling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<?php include 'includes/globalHead.html' ?>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/res/css/homepage.css">
     <!-- Javascript/Jquery imports here -->
     <script>
