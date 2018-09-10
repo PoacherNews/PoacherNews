9dbc1ead11639bb72cd6d672460f84358e84a8f2
Colin Stevens <colin@colinjstevens.com>
2018-09-10 15:40:39 -0500

    Fixed non-admins seeing admin nav links

--- includes/toolsNav.php
+++ includes/toolsNav.php
@@ -2,8 +2,14 @@
     <ul>
 <!--         <li><a <?php print($toolsTab === "editorHistory" ? 'class="active"' : ''); ?> href="/editorHistory.php">Editor History</a></li> -->
         <li><a <?php print($toolsTab === "editorpage" ? 'class="active"' : ''); ?> href="/editorpage.php">Editor Page</a></li>
-        <li><a <?php print($toolsTab === "usermanagement" && $_SESSION['usertype'] === 'A' ? 'class="active"' : ''); ?> href="/userManagement.php">Manage Users</a></li>
-        <li><a <?php print($toolsTab === "articlemanagement" && $_SESSION['usertype'] === 'A' ? 'class="active"' : ''); ?> href="/articleManagement.php">Manage Articles</a></li>
-        <li><a <?php print($toolsTab === "commentmanagement" && $_SESSION['usertype'] === 'A' ? 'class="active"' : ''); ?> href="/commentManagement.php">Manage Comments</a></li>
+        <?php
+        	if($_SESSION['usertype'] === 'A') {
+        		print '
+		        <li><a '.($toolsTab === "usermanagement" ? 'class="active"' : '').' href="/userManagement.php">Manage Users</a></li>
+		        <li><a '.($toolsTab === "articlemanagement" ? 'class="active"' : '').' href="/articleManagement.php">Manage Articles</a></li>
+		        <li><a '.($toolsTab === "commentmanagement" ? 'class="active"' : '').' href="/commentManagement.php">Manage Comments</a></li>
+		        ';
+		    }
+		?>
     </ul>
 </div>
