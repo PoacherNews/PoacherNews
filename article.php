893bb78d1a9033f1bd5f55fa5e3a036ebeccc9c8
Roland Oruche <rolandoruche@mwc-013123.dhcp.missouri.edu>
2018-09-24 15:10:56 -0500

    Another freaking mistake

--- article.php
+++ article.php
@@ -182,7 +182,7 @@
                     </p>
                 </span>
                 <div class="articleImage">
-                    <img src="<?php print "/res/img/articlePictures/".$articleData['ArticleID']."/".$articleData['ArticleImage']."/"; ?>"/>
+                    <img src="<?php print "/res/img/articlePictures/".$articleData['ArticleID']."/".$articleData['ArticleImage']; ?>"/>
                 </div>
                 <p class="articleBody"><?php print nl2br($articleData['Body']); ?></p>
             </section>
