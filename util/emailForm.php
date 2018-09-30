e8ae89d434091e4ec61226b3ae1d89c42aea504b
Jacob Ross <jross1818@gmail.com>
2018-09-30 16:00:57 -0500

    more code cleanup

--- util/emailForm.php
+++ util/emailForm.php
@@ -1,12 +1,10 @@
 <?php
 if(isset($_POST['email'])) {
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "poachernews@gmail.com";
     $email_subject = "Feedback Form Sent";
  
     function died($error) {
-        // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
@@ -77,7 +75,6 @@ $headers = 'From: '.$email_from."\r\n".
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
-<!-- include your own success html here -->
  
 Thank you for contacting us. We will be in touch with you very soon.
  
