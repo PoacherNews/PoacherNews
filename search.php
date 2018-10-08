a4d2511a79a2dac9c4a40e76addb7bb4ffbf21c7
dma3mf <dma3mf@mail.missouri.edu>
2018-10-07 23:20:07 -0500

    Added search by date

--- search.php
+++ search.php
@@ -25,8 +25,25 @@
                 <h1>Search Results</h1>
                 <form class="search" action="" method="GET">
                     <?php
-                        print "<input type=\"text\" id=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query\" value={$_GET['query']}>";
+                        
+                        print "<input type=\"text\" class=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query\" value={$_GET['query']}>";
+                        //print "<input type=\"date\" id=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query2\" value={$_GET['query2']}>";         
                     ?>
+                    
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    
+                    <div id="advancedSearch">
+                        <?php
+                            
+                            print "<input type=\"date\" class=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query2\" value={$_GET['query2']}>";
+                            print "<input type=\"date\" class=\"searchInput\" placeholder=\"What are you looking for?\" name=\"query3\" value={$_GET['query3']}>";
+    
+                        ?>
+                    </div>
+                    
                     <div class="buttonFlex">
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </div>
@@ -38,30 +55,76 @@
                 </div>
                 <div class="dropdown-content">
 				<?php
-                    print "<a href=\"search.php?query={$_GET['query']}&sort=Relevancy\">Relevancy</a>";
-                    print "<a href=\"search.php?query={$_GET['query']}&sort=Name\">Name</a>";
-                    print "<a href=\"search.php?query={$_GET['query']}&sort=Views\">Views</a>";
+                    
+                    print "<a href=\"search.php?query={$_GET['query']}&query2={$_GET['query2']}&query3={$_GET['query3']}&sort=Relevancy\">Relevancy</a>";   
+                    print "<a href=\"search.php?query={$_GET['query']}&query2={$_GET['query2']}&query3={$_GET['query3']}&sort=Name\">Name</a>";
+                    print "<a href=\"search.php?query={$_GET['query']}&query2={$_GET['query2']}&query3={$_GET['query3']}&sort=Views\">Views</a>";
+                    print "<a href=\"search.php?query={$_GET['query']}&query2={$_GET['query2']}&query3={$_GET['query3']}&sort=Date\">Date</a>";
 				?>
                 </div>
             </div> 
-
         </div>
+        
+        <button type: "button" onclick="showAdvancedSearch()">Restrict by Date Range </button>
+       
+        
+        <script>
+            function showAdvancedSearch(){
+                var x = document.getElementById("advancedSearch");
+                if (x.style.display === "none") {
+                    x.style.display = "flex"
+                } else {
+                    x.style.display = "none";
+                }
+            }
+        
+        </script>
 
         <div class="resultFlex">
                 <div class="testFlex">
                 <?php
                     $query = $_GET['query'];
-                    $sort = $_GET['sort'];
+                    
+                    //print $query;
+                    
+                    $query2 = $_GET['query2'];
+                    
+                    //print $query2;
+                        
+                    $query3 = $_GET['query3'];
+    
+                    //print $query3;
+                    
+        
+                    if(!isset($_GET['sort'])){
+                        $sort="default";
+                    }
+                    else{
+                        $sort = $_GET['sort'];
+                    }
+                    
+                    /*
+                    if(!$_GET['sort']){
+                        $sort = "fortnitefree";
+                    }
+                    else{
+                         $sort = $_GET['sort'];
+                    }
+                   
+                    print $sort;
+                   */
+                   
+                    
+                    
                     $min_length = 1;
 					if(!(isset($_GET['sort']))) {
 						if(strlen($query) >= $min_length){ 
 							$query = htmlspecialchars($query); 
 							$query = mysqli_real_escape_string($db, $query);
 
-							$raw_results = "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`ArticleImage` LIKE '%".$query."%'))";
+							$raw_results = "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`ArticleImage` LIKE '%".$query."%')) ";
 							$test = mysqli_query($db, $raw_results);
 							echo "<div class='search-content'>";
-
 							if(mysqli_num_rows($test) > 0){ 
 								while($results = mysqli_fetch_array($test, MYSQLI_ASSOC)){
 									print   "<div class='flexRow'>
@@ -89,7 +152,7 @@
                     } else {
                         switch($sort) {
                             case "Relevancy":
-                                $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`ArticleImage` LIKE '%".$query."%')) ORDER BY PublishDate DESC") or die(mysql_error());
+                                 $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND ((`Headline` LIKE '%".$query."%') OR (`ArticleImage` LIKE '%".$query."%')) ORDER BY PublishDate DESC") or die(mysql_error());
                                 echo "<div class='search-content'>";
                                 if(mysqli_num_rows($raw_results) > 0){ 
                                     while($results = mysqli_fetch_array($raw_results)){
@@ -163,9 +226,39 @@
                                     print "<b>No results.</b>";
                                 }
                                 break;
+                               
+                            case "Date":
+                               $raw_results = mysqli_query($db, "SELECT * FROM Article WHERE IsSubmitted = 1 AND IsDraft = 0 AND (PublishDate BETWEEN '$query2' AND '$query3') ORDER BY Views DESC") or die(mysql_error());
+                                echo "<div class='search-content'>";
+                                if(mysqli_num_rows($raw_results) > 0){ 
+                                    while($results = mysqli_fetch_array($raw_results)){
+                                        print   "<div class='flexRow'>
+                                                    <div class='imgFlex'>
+                                                        <a href=\"article.php?articleid={$results['ArticleID']}\">
+                                                            <img src=".$results['ArticleImage']." class='image' height='120' width='140'>
+                                                        </a>
+                                                    </div>
+                                                    <div class='spanFlex'>
+                                                        <span class='tip'>
+                                                            <a href=\"article.php?articleid={$results['ArticleID']}\">{$results['Headline']}</a>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <hr class='searchHr'>";
+                                    }
+                                   
+                                }
+                                else {
+                                    print "<b>No results.</b>";
+                                    
+                                }
+                                break;
+                            
+                                
                         }//End of switch
                     }//End of else
                 ?>
+                    
                 </div>
         </div>
     </div>
