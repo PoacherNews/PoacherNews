606f7096d4dba82656ad70b4dc166e0c4521434a
Jacob Ross <jross1818@gmail.com>
2018-11-08 11:45:15 -0600

    added styling to feedback form and changed the way it works a little

--- feedback.php
+++ feedback.php
@@ -22,46 +22,16 @@
         <p>
             For feedback or more information, please contact:<a id="feedbackContactLink" href="mailto:poachernews@gmail.com">poachernews@gmail.com</a>
         </p>
-        <form name="contactform" method="post" action="util/emailForm.php">
-            <table width="450px">
-            <tr>
-            <td valign="top">
+        <form name="contactform" class="formTable" method="post" action="util/emailForm.php">
             <label for="first_name">First Name *</label>
-            </td>
-            <td valign="top">
             <input  type="text" name="first_name" maxlength="50" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
             <label for="last_name">Last Name *</label>
-            </td>
-            <td valign="top">
             <input  type="text" name="last_name" maxlength="50" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
             <label for="email">Email Address *</label>
-            </td>
-            <td valign="top">
             <input  type="text" name="email" maxlength="80" size="30">
-            </td>
-            </tr>
-            <tr>
-            <td valign="top">
             <label for="comments">Comments *</label>
-            </td>
-            <td valign="top">
-            <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-            </td>
-            </tr>
-            <tr>
-            <td colspan="2" style="text-align:center">
+            <textarea  name="comments" maxlength="2000" cols="50" rows="6" id="comments"></textarea>
             <input type="submit" value="Submit">  
-            </td>
-            </tr>
-            </table>
         </form>
     </div>
     <?php include('includes/footer.html'); ?>
