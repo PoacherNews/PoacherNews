072641db4c8c69db70e484172387eb172004c224
Jacob Ross <jross1818@gmail.com>
2018-12-01 20:00:51 -0600

    added more seamless error messaging

--- feedback.php
+++ feedback.php
@@ -1,7 +1,8 @@
 <?php
     include 'util/loginCheck.php';
-    include 'util/emailForm.php';
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -10,6 +11,17 @@
 	<?php include 'includes/globalHead.html' ?>
     <link rel="stylesheet" type="text/css" href="res/css/feedback.css">
 </head>
+<style>
+    .errorMessage {
+                margin: 5px 0px 5px 0px;
+                padding: 10px;
+                border-radius: 2px;
+                font-weight: bold;
+                box-shadow: 0 0 0 1px #e0b4b4 inset;
+                background-color: #fff6f6;
+                color: #9f3a38;
+            }
+</style>
 <body>
     <?php 
         include 'includes/header.php';
@@ -21,17 +33,22 @@
             The staff here at <span class="text-emphasis">The Poacher</span> really appreciate what our users think about our content. Let us know how our website is doing!
         </p>
         <p>
-            For feedback or more information, please contact:<a id="feedbackContactLink" href="mailto:poachernews@gmail.com">poachernews@gmail.com</a>
+            For feedback or more information, please contact:<a id="feedbackContactLink" href="mailto:poachernews@gmail.com">poachernews@gmail.com or fill out the form below!</a>
         </p>
-        <form name="contactform" class="formTable" method="post" action="util/emailForm.php">
-            <label for="first_name">First Name *</label>
-            <input class="feedbackInput" type="text" name="first_name" maxlength="50" size="30">
+        <?php
+            if (isset($error)) {
+                echo "<p class='errorMessage'>$error</p>";
+            }
+        ?>
+        <form name="contactform" class="formTable" method="post" action="/util/emailForm.php">
+            <label for="first_name">First Name *</label >
+            <input class="feedbackInput" type="text" name="first_name" maxlength="50" size="30" >
             <label for="last_name">Last Name *</label>
-            <input class="feedbackInput" type="text" name="last_name" maxlength="50" size="30">
+            <input class="feedbackInput" type="text" name="last_name" maxlength="50" size="30" >
             <label for="email">Email Address *</label>
-            <input class="feedbackInput" type="text" name="email" maxlength="80" size="30">
+            <input class="feedbackInput" type="text" name="email" maxlength="80" size="30" >
             <label for="comments">Comments *</label>
-            <textarea class="feedbackInput" name="comments" maxlength="2000" cols="50" rows="6" id="comments"></textarea>
+            <textarea class="feedbackInput" name="comments" maxlength="2000" cols="50" rows="6" id="comments" ></textarea>
             <input class="feedbackSubmit" type="submit" value="Submit">  
         </form>
     </div>
